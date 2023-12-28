960d2220cad0755600bc7948ecc3aa90c92f28e6
Brian Adams <albashiroh.programming@gmail.com>
2023-12-28 08:43:53 +0700

    use route chaining

--- src/controller/educationController.js
+++ src/controller/educationController.js
@@ -1,17 +1,26 @@
 const educationsMessage = { message: "OK from Educations Page", status: 200 };
 
+// GET METHOD EDUCATIONS
 const get =  (req, res) => {
     res.status(200).json(educationsMessage);
   }
+
+// POST METHOD EDUCATIONS
 const post =  (req, res) => {
     res.status(200).json(educationsMessage);
   }
+
+// PUT METHOD EDUCATIONS
 const put =  (req, res) => {
     res.status(200).json(educationsMessage);
   }
+
+// PATCH METHOD EDUCATIONS
 const patch =  (req, res) => {
     res.status(200).json(educationsMessage);
   }
+
+// DELETE METHOD EDUCATIONS
 const remove =  (req, res) => {
     res.status(200).json(educationsMessage);
   }
--- src/router/education.js
+++ src/router/education.js
@@ -1,21 +1,17 @@
 import express from "express";
-import educationController from '../controller/educationController.js'
+import educationController from "../controller/educationController.js";
 
 const routerEducation = express.Router();
 
-// GET METHOD EDUCATIONS
-routerEducation.get("/educations",educationController.get);
+routerEducation
+  .route("/educations")
+  .get(educationController.get)
+  .post(educationController.post);
 
-// POST METHOD EDUCATIONS
-routerEducation.post("/educations",educationController.post);
-
-// PUT METHOD EDUCATIONS
-routerEducation.put("/educations/:id",educationController.put);
-
-// PATCH METHOD EDUCATIONS
-routerEducation.patch("/educations/:id",educationController.patch);
-
-// DELETE METHOD EDUCATIONS
-routerEducation.delete("/educations/:id",educationController.remove);
+routerEducation
+  .route("/educations/:id")
+  .put(educationController.put)
+  .patch(educationController.patch)
+  .delete(educationController.remove);
 
 export { routerEducation };
