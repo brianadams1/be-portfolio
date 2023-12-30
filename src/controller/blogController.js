2435dca33cebb84de9cf93f82845795c642a9de7
Brian Adams <albashiroh.programming@gmail.com>
2023-12-30 10:30:11 +0700

    handle get-blogs and get-blog

--- src/controller/blogController.js
+++ src/controller/blogController.js
@@ -1,24 +1,41 @@
 import { Prisma } from "../app/prisma.js";
 
-const blogsMessage ={message: "OK from Blogs Page", status: 200} ;
+const blogsMessage = { message: "OK from Blogs Page", status: 200 };
 
 /*
 ive tried so hard and got so fast but in the end it doesnt even matter
 
-*/ 
+*/
 
-// GET METHOD BLOGS
-const get = async (req, res) => {
-  const blog = await Prisma.blog.findMany()
+// GETALL METHOD BLOGS
+const getAll = async (req, res) => {
+  // FIND MANY >> ambil semua blog
+  const datas = await Prisma.blog.findMany();
   res.status(200).json({
-    message: blogsMessage,
-    blog: blog
+    message: "BERHASIL MENDAPATKAN SEMUA DATA BLOG",
+    blogs: datas,
+  });
+};
+// GET METHOD BLOG BY ID
+const get = async (req, res) => {
+  let id = req.params.id;
+  id = parseInt(id);
+  // FINDUNIQUE >> AMBIL BLOG BERDASARKAN ID
+  const blog = await Prisma.blog.findUnique({
+    where: { id },
+  });
+  res.status(200).json({
+    message: "BERHASIL MENDAPATKAN DATA BLOG DENGAN ID",
+    blog: blog,
   });
 };
 
 // POST METHOD BLOGS
 const post = (req, res) => {
-  res.status(200).json(blogsMessage);
+  
+  res.status(200).json({
+    message: "BERHASIL SAVE BLOG BARU",
+  });
 };
 
 // PUT METHOD BLOGS
@@ -37,6 +54,7 @@ const remove = (req, res) => {
 };
 
 export default {
+  getAll,
   get,
   post,
   put,
--- src/router/blog.js
+++ src/router/blog.js
@@ -3,7 +3,12 @@ import blogController from "../controller/blogController.js";
 
 const routerBlogs = express.Router();
 
-routerBlogs.route("/blogs").get(blogController.get).post(blogController.post);
+// GET ALL BLOGS
+routerBlogs.get("/blogs", blogController.getAll)
+// GET BLOG BY ID
+routerBlogs.get('/blog/:id',blogController.get)
+// SAVE NEW BLOG
+routerBlogs.post('/blog',blogController.post);
 
 routerBlogs
   .route("/blogs/:id")
