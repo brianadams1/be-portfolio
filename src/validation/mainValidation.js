6028ebf801bde4615340a492021555cd48348393
Brian Adams <albashiroh.programming@gmail.com>
2024-01-03 10:47:37 +0700

    little adjustmentcomment

--- src/validation/mainValidation.js
+++ src/validation/mainValidation.js
@@ -1,9 +1,11 @@
 import Joi from "joi";
+// import JoiDate from '@hapi/joi-date'
 
 const isID = Joi.number().positive().required().label("ID");
 const isString100 =  Joi.string().trim().min(3).max(100)
 const isString255 =  Joi.string().trim().min(3).max(255)
 const isYear = Joi.number().positive()
+// const isDate = Joi.date()
 
 export {
     isID, isString100, isString255, isYear
