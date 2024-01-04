e9947e6df120ab3971963d031509d68719cbb5a7
Brian Adams <albashiroh.programming@gmail.com>
2024-01-04 10:38:03 +0700

    reroute

--- src/router/public.js
+++ src/router/public.js
@@ -1,11 +1,10 @@
 import express  from "express";
 import blogController from "../controller/blogController.js";
-import { routerAuth } from "./auth.js";
 import authController from "../controller/authController.js";
 
 export const routerPublic = express.Router()
 
-routerAuth.post("/login", authController.login);
+routerPublic.post("/login", authController.login);
 
 // get all blogs
 routerPublic.get('/blogs', blogController.getAll)
