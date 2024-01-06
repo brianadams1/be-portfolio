55256e7551782cdbd0321781ae9ed6a8e03b03c6
Brian Adams <albashiroh.programming@gmail.com>
2024-01-06 08:47:49 +0700

    handle profile dummy data

--- src/controller/profileController.js
+++ src/controller/profileController.js
@@ -1,19 +1,41 @@
+import { Prisma } from "../app/prisma.js";
+
 const profileMessage = { message: "OK from Profile Page", status: 200 };
 
 // GET METHOD PROFILE
-const get = (req, res) => {
-  const data = {
-    firstname: "John",
-    lastname: "Doe",
-    email: "john.doe@gmail.com",
-    age: 18,
-  };
-  res.status(200).json(data);
+const get = async (req, res, next) => {
+  try {
+    
+    // CHECK TO DATABASE
+    let profile = await Prisma.profile.findFirst()
+  
+    // IF DATA IS EMPTY, SEND DUMMY DATA
+    if(!profile) {
+      profile = 'DUMMY DATAS'
+    }
+  
+    // IF DATA IS EXIST, SEND DATA
+    res.status(200).json({
+      message:"SUCCESS GET PROFILE DATA",
+      data: profile
+    });
+  } catch (error) {
+    next(error)
+  }
 };
 
 // PUT METHOD PROFILE
-const put = (req, res) => {
-  res.status(200).json(profileMessage);
+const put = (req, res, error) => {
+  try {
+    
+    res.status(200).json(
+      {
+        message: "",
+      }
+    );
+  } catch (error) {
+  next(error)    
+  }
 };
 
 
