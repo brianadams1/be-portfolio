77012d480120889e6d296123e8485966c6652e81
Brian Adams <albashiroh.programming@gmail.com>
2024-01-20 10:19:19 +0700

    add svg and edit date

--- src/validation/projectValidation.js
+++ src/validation/projectValidation.js
@@ -4,8 +4,8 @@ import { isString100, isString255, isText, isURI } from "./mainValidation.js";
 export const isProject = Joi.object({
     title: isString255.required().label("Title"),
     description: isText.required().label("Description"),
-    startDate: Joi.date().less('now').required().label("Start Date"),
-    endDate: Joi.date().less('now').label("Start Date"),
+    startDate: Joi.date().max('now').required().label("Start Date"),
+    endDate: Joi.date().min(Joi.ref('startDate')).max('now').label("Start Date"),
     status: Joi.string().valid('ON_PROGRESS', 'MAINTENANCE', 'COMPLETE').label("Project Status"),
     url: isURI.label("URL"),
     github: isURI.label("Github"),
