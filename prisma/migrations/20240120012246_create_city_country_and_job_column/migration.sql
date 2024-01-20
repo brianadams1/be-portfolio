d2ca96c3f3f43dbf74148802829684954ea240d9
Brian Adams <albashiroh.programming@gmail.com>
2024-01-20 09:09:36 +0700

    create additional info row for profile

--- prisma/migrations/20240120012246_create_city_country_and_job_column/migration.sql
+++ prisma/migrations/20240120012246_create_city_country_and_job_column/migration.sql
@@ -1,4 +1,41 @@
 -- CreateTable
+CREATE TABLE `user` (
+    `name` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(200) NOT NULL,
+    `token` VARCHAR(255) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`email`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `profile` (
+    `email` VARCHAR(100) NOT NULL,
+    `firstName` VARCHAR(100) NOT NULL,
+    `lastName` VARCHAR(100) NOT NULL,
+    `dob` DATE NOT NULL,
+    `address` TEXT NOT NULL,
+    `city` VARCHAR(100) NOT NULL,
+    `country` VARCHAR(100) NOT NULL,
+    `job` VARCHAR(100) NOT NULL,
+    `bio` TEXT NULL,
+    `website` VARCHAR(100) NULL,
+    `github` VARCHAR(100) NULL,
+    `gitlab` VARCHAR(100) NULL,
+    `instagram` VARCHAR(100) NULL,
+    `facebook` VARCHAR(100) NULL,
+    `twitter` VARCHAR(100) NULL,
+    `linkedin` VARCHAR(100) NULL,
+    `discord` VARCHAR(100) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`email`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `blog` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(255) NOT NULL,
