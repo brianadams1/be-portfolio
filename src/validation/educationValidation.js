719d1a82fab9d2019b8e6f31913f7e142ffad53b
Brian Adams <albashiroh.programming@gmail.com>
2024-01-27 09:43:50 +0700

    fix education validate

--- src/validation/educationValidation.js
+++ src/validation/educationValidation.js
@@ -3,13 +3,11 @@ import { isString100, isYear } from "./mainValidation.js";
 
 // OBJECT VALIDATION
 const isEducation = Joi.object({
-    institutionName: isString100.required().label("Institution Name"),
-    startYear: isYear.required().label("Start Year"),
-    endYear: isYear,
-    major: isString100,
-    degree: isString100
-  });
+  institutionName: isString100.required().label("Institution Name"),
+  startYear: isYear.required().label("Start Year"),
+  endYear: isYear.label("End Year").allow(""),
+  major: Joi.string().trim().label("Major").allow(""),
+  degree: Joi.string().trim().label("Degree").allow(""),
+});
 
-export {
-    isEducation
-}
+export { isEducation };
